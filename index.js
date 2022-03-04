3498ae45ccdcbad64861192087b0133bb59dc552
bilalozair <bo569@nyu.edu>
2022-03-05 01:35:27 +0500

    commit tree ec8365c7fb4b5376a42ea8331fef23109e370202

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+// let driverNames =['Antonia', 'Nuru', 'Amari', 'Mo'];
+// test = [...driverNames]
+
+const returnFirstTwoDrivers = function (driverNames) {
+     return driverNames.slice(0,2);
+}
+const returnLastTwoDrivers = function(driverNames) {
+    return driverNames.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (multiplier) {
+    return function (fare) {
+        return multiplier*fare;
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(driverNames,returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(driverNames);
+}
